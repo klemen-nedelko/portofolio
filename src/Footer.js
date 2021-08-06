df4bc595da1550dda013e194d1c292d2bf3e6f3f
Klemen Nedelko <klemen.nedelko2@gmail.com>
2021-08-06 12:00:56 +0100

    change background color and color

--- src/Footer.js
+++ src/Footer.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import linkedin from './img/linkedin.png'
 import github from './img/github.png'
+import mail from './img/mail.png'
 import './App.css';
+import {BrowserRouter as Router,Link,Route,Switch} from 'react-router-dom';
 
 function Footer(){
     return (
       <div className="Footer">
-        <div className="footer-left-side">
-          left side
-          </div>
           <div className="copyright">
           Copyright &copy; <script>document.write(new Date().getFullYear())</script> Klemen Nedelko All Rights Reserved
           </div>
           <div className="footer-right-side">
-            <img src={linkedin} className="social" alt="social"/>
-            <img src={github} className="social" alt="social"/>
+            <a href="https://www.linkedin.com/in/klemen-nedelko/"><img src={linkedin} className="social" alt="social"/>linkedin</a>
+            <a href="https://github.com/klemen-nedelko"><img src={github} className="social" alt="social"/>GitHub</a>
+            <a href="https://github.com/klemen-nedelko"><img src={mail} className="social" alt="social"/>Email</a>
+
           </div>
     </div>
   );
