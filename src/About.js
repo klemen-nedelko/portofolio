4e34a30858889e8ab0cf3817c6bb9eb4e6ac2500
Klemen Nedelko <klemen.nedelko2@gmail.com>
2021-08-08 18:14:45 +0100

    add new

--- src/About.js
+++ src/About.js
@@ -3,23 +3,15 @@ import './App.css';
 
 function About() {
     return (
-      <div className="About">
-       <p className="name-surname">KLEMEN NEDELKO</p>
-       <p className="about-me">Web developer, specialized on frontend.
-       Working on some projects
-       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed 
-       do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
-       esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-       </p>
-       <p className="edjucation">Web developer. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-       Duis aute irure dolor in reprehenderit in voluptate 
-       velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-       <p className="languages">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-       Duis aute irure dolor in reprehenderit in voluptate 
-       velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      </div>
+    <div className="About">
+        <p className="name-surname">KLEMEN NEDELKO
+      </p>
+        <p className="about-me">Spletni razvijalec, študent informatike. Usmerjen v razvoj in oblikovanje spletne strani.<br/>
+      </p>
+        <p className="languages">
+          C++
+      </p>
+    </div>
     );
   }
   
--- src/Projects.js
+++ src/Projects.js
@@ -1,16 +1,50 @@
 import React from 'react';
 import './App.css';
+import moviez from './img/moviez_logo.png';
+import quiz from './img/logo_quiz.png';
+import restfulapi from './img/restfulapi-logo.png';
+import napoved from './img/napoved-delnic.png';
+
+
 function Projects() {
     return (
       <div className="Projects">
-       <p className="finish-projects">Finish projectsLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-       Duis aute irure dolor in reprehenderit in voluptate 
-       velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-       <p className="open-projects">Open projectsLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-       Duis aute irure dolor in reprehenderit in voluptate 
-       velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p className="finish-projects">
+          <p className="title">Dokončani projekti</p>
+          <div className="project">
+          <a href="https://github.com/klemen-nedelko/Quiz-App"><img src={quiz}alt='logo-quizapp'/></a>
+                <p><strong>Quiz App</strong>
+                  <br/>
+                  <br/>
+                    Preprosta kviz aplikacija, izdelana s pomočjo PyQt5. Vprašanja in odgovore jemlje s pomočjo API-ja, s spletne strani opendtb.
+                </p>
+          </div>
+          <div className="project">
+          <a href="https://github.com/klemen-nedelko/Python-Flask-RestplusAPI"><img src={restfulapi} alt='logo-restfulapi'/></a>
+                <p><strong>Predvidevanje cene delnic</strong>
+                  <br/>
+                  <br/>
+                    Lasten API s pomočjo knjižnice Restplus. Izdelano v Flask-u. Zapisan primer shop API-ja. 
+                </p>
+          </div>
+          <div className="project">
+          <a href="https://github.com/klemen-nedelko/Stock-prediction"><img src={napoved}alt='logo-napove'/></a>
+                <p><strong>Predvidevanje cene delnic</strong>
+                  <br/>
+                  <br/>
+                    S pomočjo umetne inteligence napovedati ceno delnice za nasljednji dan, dneve. 
+                </p>
+          </div>
+      </p>
+      <p className="title">Odprti projekti / nedokončani</p>
+        <p className="finish-projects"> 
+          <img src={moviez}alt='logo-moviez'/> 
+            <p><strong>MOVIEZ</strong>
+            <br/>
+            <br/>
+            Moviez : mobilna aplikacija, ki s pomočjo všečkanja filmov in serij, izbereš pravo serijo s prijatleji ali partnerjem za večer.
+          </p>
+        </p>
       </div>
     );
   }
